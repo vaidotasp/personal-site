503036cb055cb6922959dd817897f36656281feb
vaidotasp <vaidotasp@gmail.com>
2019-02-16 15:44:09 -0500

    typography setup

---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from "typography";
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  headerFontFamily: ["Work Sans", "Helvetica Neue", "Segoe UI", "Helvetica", "Arial", "sans-serif"],
+  bodyFontFamily: ["Quattrocento Sans", "Georgia", "serif"]
+});
+
+export default typography;
