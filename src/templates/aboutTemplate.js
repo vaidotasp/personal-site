7deebab6a757d89eeb3eee52fcd4301a27170d74
vaidotasp <vaidotasp@gmail.com>
2019-02-28 20:32:04 -0500

    remove template

--- src/templates/aboutTemplate.js
+++ src/templates/aboutTemplate.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { graphql } from "gatsby";
+// import React from "react";
+// import { graphql } from "gatsby";
 
-export default function Template({ data }) {
-  const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
-  return (
-    <div className="page-container">
-      <div className="post">
-        <h1>Title</h1>
-        <div className="content" dangerouslySetInnerHTML={{ __html: html }} />
-      </div>
-    </div>
-  );
-}
+// export default function Template({ data }) {
+//   const { markdownRemark } = data;
+//   const { frontmatter, html } = markdownRemark;
+//   return (
+//     <div className="page-container">
+//       <div className="post">
+//         <h1>Title</h1>
+//         <div className="content" dangerouslySetInnerHTML={{ __html: html }} />
+//       </div>
+//     </div>
+//   );
+// }
 
-export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { date: { eq: $path } }) {
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-      }
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query($path: String!) {
+//     markdownRemark(frontmatter: { date: { eq: $path } }) {
+//       html
+//       frontmatter {
+//         date(formatString: "MMMM DD, YYYY")
+//       }
+//     }
+//   }
+// `;
