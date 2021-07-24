92812fac9c03a13e69ea49d0a0c9ae50ff310eb6
Vaidotas Piekus <vaidotasp@gmail.com>
2021-07-24 10:25:09 +0200

    svelte things for main pages

---
+++ src/components/Now.svelte
@@ -0,0 +1,60 @@
+<div class="now">
+  <h4>Now</h4>
+  <p>
+    Inspired by Derek Sivers<!-- -->
+    <a
+      class="now-module--link--3_nao"
+      target="_blank"
+      href="https://nownownow.com/about"
+      rel="noopener noreferrer">nownownow&nbsp;</a
+    >page, here is what I am doing right now as of <!-- -->April 15, 2021<!-- -->:
+  </p>
+
+  <ul>
+    <li>
+      <span>Just moved to Germany, lots of things to setup and learn.</span>
+    </li>
+    <li>
+      <span>Experimenting with Go language by building a weather forecast CLI tool.</span>
+    </li>
+    <li>
+      <span
+        >Diving deep in React Testing Library and other testing approaches for React applications.</span
+      >
+    </li>
+    <li>
+      <span>Writing a blog post about legacy code why it is awesome to work on.</span>
+    </li>
+  </ul>
+</div>
+
+<style>
+  h4 {
+    font-size: 1.2rem;
+  }
+  .now {
+    border-left: 1px solid rgba(35, 26, 26, 0.102);
+    padding-left: 50px;
+  }
+
+  ul {
+    margin: 0;
+  }
+
+  li {
+    font-size: 1rem;
+    padding-left: 0;
+    list-style-type: square;
+    line-height: 30px;
+  }
+
+  p {
+    line-height: 30px;
+    font-size: 1.1rem;
+  }
+
+  a {
+    color: #00f;
+    text-decoration: none;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to yet another rewrite</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import InfoCard from "../components/InfoCard.svelte";
+  import About from "../components/About.svelte";
+  import Now from "../components/Now.svelte";
+</script>
+
+<InfoCard />
+<div class="main">
+  <About />
+  <Now />
+</div>
+
+<style>
+  .main {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 50px;
+    max-width: 960px;
+    margin: 0 auto;
+  }
+
+</style>
