3327a05bd38bacb07a297053d6e40f7e6ba04a4f
Vaidotas Piekus <vaidotasp@gmail.com>
2023-02-01 20:00:58 +0100

    cleanupsome

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,12 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-// import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
 	const posts = await getCollection("posts");
 	return rss({
-		title: "SITE_TITLE",
-		description: "SITE_DESCRIPTION",
-		site: "http://www.google.com",
+		title: "VP Posts",
+		description: "VP Posts Corner",
+		site: "http://www.vaidotas.dev",
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/posts/${post.slug}/`
